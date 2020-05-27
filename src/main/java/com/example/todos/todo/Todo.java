a0ad7d2ea442311b7728d52540157fd2d8e10178
Juaquin Gomez <juaquin.gomez619@gmail.com>
2020-05-27 17:25:49 -0400

    Initial commit

---
+++ src/main/java/com/example/todos/todo/Todo.java
@@ -0,0 +1,31 @@
+package com.example.todos.todo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Enumerated(EnumType.ORDINAL)
+
+    private long id;
+    private String description;
+    private boolean complete;
+    private String title;
+
+    public Todo(String description, boolean complete, String title){
+        this.description = description;
+        this.complete = complete;
+        this.title = title;
+    }
+
+
+}
